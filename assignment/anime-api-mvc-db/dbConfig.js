7f05a3324bb0f69007880f6ab44a0115391c4e8f
xicoldes <bigbyme@gmail.com>
2025-07-22 02:47:52 +0800

    test if its connected to github

---
+++ assignment/anime-api-mvc-db/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  trustServerCertificate: true,
+  options: {
+    port: parseInt(process.env.DB_PORT), // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
